7c709f619a6ee2faf92b7cd99ef986271e78fe44
twkang1332-ktw <twkang1332@gmail.com>
2026-03-11 17:22:11 +0900

    day09-1

---
+++ day09/profile-generator/profile.js
@@ -0,0 +1,19 @@
+var name = "taewon"
+console.log(name)
+var name = "taewon0"
+console.log(name)
+name = "taewon1"
+console.log(name)
+
+let age = "27"
+console.log(age)
+// let age = "28"       // 변수 재선언 불가
+age = "28"      // 재할당 가능
+console.log(28)
+
+const major = "science"
+console.log(major)
+// const major = "math"     // 재할당 불가
+
+
+
