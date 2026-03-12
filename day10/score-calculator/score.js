d42d3be4c4dab87eed70cf989400ca52dc645751
twkang1332-ktw <twkang1332@gmail.com>
2026-03-12 21:10:23 +0900

    day10 과제(도전과제 포함)

--- day10/score-calculator/score.js
+++ day10/score-calculator/score.js
@@ -2,13 +2,30 @@ const maxScore = 100;
 
 let input = prompt("점수를 입력하세요.");
 let score = Number(input);
-score += 5;     // 보너스 점수 +5
+
 var grade;
-console.log(typeof input)
+
+// 입력 유효성 검사
+if (isNaN(score) || score < 0 || score > 100) {
+    console.log("Invalid score! Please enter a number between 0 and 100.")
+}
+// 단항 부정
+// if (!isNaN(score) && score >= 0 && score <= 100) {
+//     console.log("Invalid score! Please enter a number between 0 and 100.")
+// }
+
+// 추가 연산
+score++;        // 단항 산술
+score += 5;     // 보너스 점수 +5
+score *=1.1;    // 복합 대입
+
+
+// 복잡한 조건1
+if (score > maxScore) score = maxScore;
 
 
 // 등급
-if (score >= 100) {
+if (score === 100) {
     grade = "S";
 } else if (score >= 90) {
     grade = "A";
@@ -23,13 +40,15 @@ if (score >= 100) {
 }
 
 // 합불 여부
-let status = (score >= 60) ? "Pass" : "Fail"
+let status = (score >= 60) ? "Pass" : "Fail";
 
 // 등급별 메세지
 let message;
 switch (grade) {
     case "S":
-        message = "Super!!";
+        if (score === 100){
+        message = "Perfect Score!";
+        }   // 복잡한 조건2
         break;
     case "A":
         message = "Excellent work!";
