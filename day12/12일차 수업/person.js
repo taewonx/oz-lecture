958e3c5b2387fdb2baf05a551607f71406aa60f2
twkang1332-ktw <twkang1332@gmail.com>
2026-03-16 14:59:54 +0900

    day12 lecture

---
+++ day12/12일차 수업/person.js
@@ -0,0 +1,27 @@
+const person1 = {
+  name: "John",
+  age: 30,
+};
+const person2 = {
+  name: "Jane",
+  age: 25,
+};
+const person3 = {
+  name: "Doe",
+  age: 40,
+};
+
+// 생성자
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const person4 = new Person("alice", 35);
+const person5 = new Person("bob", 28);
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
+console.log(person4);
+console.log(person5);
