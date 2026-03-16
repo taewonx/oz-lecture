7d68e9093b17b24b1ea9f94ff43e8296d9a3aa18
twkang1332-ktw <twkang1332@gmail.com>
2026-03-16 19:06:50 +0900

    day12 challenge task 1/2

--- day12/movie-collection/movie.js
+++ day12/movie-collection/movie.js
@@ -1,6 +1,6 @@
 // 영화 목록 배열
 let movies = [{
-    title:"",
+    title:"Gladiator",
     director:"Scott",
     year:2000,
     genre:"history",
@@ -43,3 +43,71 @@ function printMovies(movies) {
 
 // 함수 출력
 printMovies(movies);
+
+// Nolan 감독의 영화 출력
+// 1. for문 으로 출력
+function nolanMovieFor(movies){
+
+    var count = movies.length
+
+    console.log("-----Nolan Moives(for)-----");
+
+    for (let i = 0; i < count; i++) {
+    const list = movies[i];
+
+    if (list.director === "Nolan") {
+    
+        console.log(list);
+    }
+}
+}
+nolanMovieFor(movies);
+
+// 2. for...in문 으로 출력
+function nolanMovieForIn(movies){
+
+    console.log("-----Nolan Moives(for...in)-----");
+
+    for (let idx in movies) {
+
+    if (movies[idx].director === "Nolan") {
+
+        console.log(movies[idx]);
+    }
+}
+}
+nolanMovieForIn(movies);
+
+// 3. for...of문 으로 출력
+function nolanMovieForOf(movies){
+
+    console.log("-----Nolan Moives(for...of)-----");
+
+    for (let movie of movies) {
+
+    if (movie.director === "Nolan") {
+
+        console.log(movie);
+    }
+}
+}
+nolanMovieForOf(movies);
+
+// 4. filter()로 출력
+function nolanMovieFilter(movies) {
+
+    console.log("-----Nolan Movies(filter)-----");
+
+    const nolanMovies = movies.filter(function(movie) {
+        return movie.director === "Nolan";
+    });
+
+    // const nolanMovies = movies.filter((movie) => movie.director === "Nolan");
+
+    console.log(nolanMovies)
+}
+nolanMovieFilter(movies);
+
+// 1. 평균 출판년도 계산
+// 2. 가장 최신 영화 찾기
+// 3. ...rest 사용하여 여러 영화 객체 한번에 추가
